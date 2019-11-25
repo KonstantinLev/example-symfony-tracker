a1952124a149c8c3dcc89c40fadf53fbe8a45a95
Konstantin <levachin@luckandroll.com>
2019-11-25 19:35:54 +0300

    added sign up confirm

--- manager/src/Model/User/Entity/User/UserRepository.php
+++ manager/src/Model/User/Entity/User/UserRepository.php
@@ -5,5 +5,6 @@ namespace App\Model\User\Entity\User;
 interface UserRepository
 {
     public function hasByEmail(Email $email): bool;
+    public function findByConfirmToken(string $token): ?User;
     public function add(User $user): void;
 }
--- manager/src/Model/User/UseCase/SignUp/Request/Handler.php
+++ manager/src/Model/User/UseCase/SignUp/Request/Handler.php
@@ -7,7 +7,7 @@ use App\Model\User\Entity\User\Email;
 use App\Model\User\Entity\User\Id;
 use App\Model\User\Entity\User\User;
 use App\Model\User\Entity\User\UserRepository;
-use App\Model\User\Service\ConfirmTokenizer;
+use App\Model\User\Service\SignUpConfirmTokenizer;
 use App\Model\User\Service\ConfirmTokenSender;
 use App\Model\User\Service\PasswordHasher;
 
@@ -16,11 +16,13 @@ class Handler
     private $users;
     private $hasher;
     private $flusher;
+    private $tokenizer;
+    private $sender;
 
     public function __construct(
         UserRepository $users,
         PasswordHasher $hasher,
-        ConfirmTokenizer $tokenizer,
+        SignUpConfirmTokenizer $tokenizer,
         ConfirmTokenSender $sender,
         Flusher $flusher
     )
