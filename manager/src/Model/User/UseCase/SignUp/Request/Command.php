461c5f7d4aaeab0f1c4edd2db0af26dea381a947
Konstantin <levachin@luckandroll.com>
2019-12-08 22:07:23 +0300

    Added sign up page, added flashes template, added form theme

--- manager/src/Model/User/UseCase/SignUp/Request/Command.php
+++ manager/src/Model/User/UseCase/SignUp/Request/Command.php
@@ -2,22 +2,30 @@
 
 namespace App\Model\User\UseCase\SignUp\Request;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 class Command
 {
     /**
      * @var string
+     * @Assert\NotBlank()
      */
     public $firstName;
     /**
      * @var string
+     * @Assert\NotBlank()
      */
     public $lastName;
     /**
      * @var string
+     * @Assert\NotBlank()
+     * @Assert\Email()
      */
     public $email;
     /**
      * @var string
+     * @Assert\NotBlank()
+     * @Assert\Length(min=6)
      */
     public $password;
 }
