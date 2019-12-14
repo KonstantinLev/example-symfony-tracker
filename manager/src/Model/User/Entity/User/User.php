d1ad1f3dc3c77b868ebd7f1a8296d55ba070a14a
Konstantin <levachin@luckandroll.com>
2019-12-15 00:58:03 +0300

    Removed redundant code

--- manager/src/Model/User/Entity/User/User.php
+++ manager/src/Model/User/Entity/User/User.php
@@ -15,7 +15,6 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class User
 {
-    private const STATUS_NEW = 'new';
     private const STATUS_WAIT = 'wait';
     public const STATUS_ACTIVE = 'active';
 
@@ -137,11 +136,6 @@ class User
     public function isActive(): bool
     {
         return $this->status === self::STATUS_ACTIVE;
-    }
-
-    public function isNew(): bool
-    {
-        return $this->status === self::STATUS_NEW;
     }
 
     public function getId(): Id
