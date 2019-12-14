3d16b3afcd6dcd3cb92ef2b4908b63fe82c94e10
Konstantin <levachin@luckandroll.com>
2019-12-15 00:46:03 +0300

    Added user console confirm command

---
+++ manager/src/Model/User/UseCase/SignUp/Confirm/Manual/Command.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Model\User\UseCase\SignUp\Confirm\Manual;
+
+class Command
+{
+    /**
+     * @var string
+     */
+    public $id;
+    public function __construct(string $id)
+    {
+        $this->id = $id;
+    }
+}
