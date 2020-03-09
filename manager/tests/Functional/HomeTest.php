165a2a14fbec3a01dec8e9d8f72e1f56a183245b
Konstantin <levachin@luckandroll.com>
2020-03-09 23:13:55 +0300

    Some edits for tests

--- manager/tests/Functional/HomeTest.php
+++ manager/tests/Functional/HomeTest.php
@@ -3,6 +3,7 @@
 namespace App\Tests\Functional;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
 
 class HomeTest extends WebTestCase
 {
@@ -10,28 +11,11 @@ class HomeTest extends WebTestCase
     {
         $client = static::createClient();
         $client->request('GET', '/');
-        $this->assertSame(302, $client->getResponse()->getStatusCode());
-        $this->assertSame('http://localhost/login', $client->getResponse()->headers->get('Location'));
-    }
-    public function testSuccess(): void
-    {
-//        $client = static::createClient([], [
-//            'PHP_AUTH_USER' => 'admin@app.test',
-//            'PHP_AUTH_PW' => 'password',
-//        ]);
-        $client = static::createClient([
-            'username' => 'admin@app.test',
-            'password' => 'password',
-        ]);
-        $client = static::makeClient(true);
-//        $client->disableReboot();
-//        $client->setServerParameters([
-//            'PHP_AUTH_USER' => 'admin@app.test',
-//            'PHP_AUTH_PW' => 'password',
-//        ]);
-        $crawler = $client->request('GET', '/');
-        //$this->assertSame(200, $client->getResponse()->getStatusCode());
-        var_dump($client->getResponse()->headers->get('Location'));
-        $this->assertContains('Hello World!', $crawler->filter('h1')->text());
+
+        $this->assertResponseRedirects(
+            'http://localhost/login',
+            Response::HTTP_FOUND,
+            sprintf('The %s secure URL redirects to the login form.', '/')
+        );
     }
 }
