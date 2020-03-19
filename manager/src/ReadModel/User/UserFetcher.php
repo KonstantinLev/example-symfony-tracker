9c61626f5b26abc866470eeab3c6414e342074e5
Konstantin <levachin@luckandroll.com>
2020-03-19 10:54:16 +0300

    Added signup auto authentication

--- manager/src/ReadModel/User/UserFetcher.php
+++ manager/src/ReadModel/User/UserFetcher.php
@@ -109,4 +109,24 @@ class UserFetcher
         $view->networks = $stmt->fetchAll();
         return $view;
     }
+
+    public function findBySignUpConfirmToken(string $token): ?ShortView
+    {
+        $stmt = $this->connection->createQueryBuilder()
+            ->select(
+                'id',
+                'email',
+                'role',
+                'status'
+            )
+            ->from('user_users')
+            ->where('confirm_token = :token')
+            ->setParameter(':token', $token)
+            ->execute();
+
+        $stmt->setFetchMode(FetchMode::CUSTOM_OBJECT, ShortView::class);
+        $result = $stmt->fetch();
+
+        return $result ?: null;
+    }
 }
