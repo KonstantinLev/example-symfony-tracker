627e6a77e0dd4465b507ad7f4eda0a34e1443cfc
Konstantin <levachin@luckandroll.com>
2020-03-23 18:31:08 +0300

    Added user edit page

--- manager/src/Model/User/Entity/User/User.php
+++ manager/src/Model/User/Entity/User/User.php
@@ -285,6 +285,12 @@ class User
         $this->role = $role;
     }
 
+    public function edit(Email $email, Name $name): void
+    {
+        $this->name = $name;
+        $this->email = $email;
+    }
+
     public function activate(): void
     {
         if ($this->isActive()) {
