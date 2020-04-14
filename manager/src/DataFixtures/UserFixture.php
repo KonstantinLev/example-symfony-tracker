ddf5e82b2ee67de8f3a9c6d2f2c3655b8c84d920
Konstantin <levachin@luckandroll.com>
2020-04-14 20:47:12 +0300

    Improved user fixture

--- manager/src/DataFixtures/UserFixture.php
+++ manager/src/DataFixtures/UserFixture.php
@@ -23,17 +23,72 @@ class UserFixture extends Fixture
     public function load(ObjectManager $manager): void
     {
         $hash = $this->hasher->hash('password');
-        $user = User::signUpByEmail(
+
+        $network = $this->createSignedUpByNetwork(
+            new Name('Ivan', 'Ivanov'),
+            'vk',
+            '666666'
+        );
+        $manager->persist($network);
+
+        $requested = $this->createSignUpRequestedByEmail(
+            new Name('Alesha', 'Popovich'),
+            new Email('requested@app.test'),
+            $hash
+        );
+        $manager->persist($requested);
+
+        $confirmed = $this->createSignUpConfirmedByEmail(
+            new Name('Dobrinja', 'Nikitich'),
+            new Email('user@app.test'),
+            $hash
+        );
+        $manager->persist($confirmed);
+
+        $admin = $this->createAdminByEmail(
+            new Name('Tugarin', 'Zmej'),
+            new Email('admin@app.test'),
+            $hash
+        );
+        $manager->persist($admin);
+
+        $manager->flush();
+    }
+
+    private function createAdminByEmail(Name $name, Email $email, string $hash): User
+    {
+        $user = $this->createSignUpConfirmedByEmail($name, $email, $hash);
+        $user->changeRole(Role::admin());
+        return $user;
+    }
+
+    private function createSignUpConfirmedByEmail(Name $name, Email $email, string $hash): User
+    {
+        $user = $this->createSignUpRequestedByEmail($name, $email, $hash);
+        $user->confirmSignUp();
+        return $user;
+    }
+
+    private function createSignUpRequestedByEmail(Name $name, Email $email, string $hash): User
+    {
+        return User::signUpByEmail(
             Id::next(),
             new \DateTimeImmutable(),
-            new Name('First', 'Last'),
-            new Email('admin@app.test'),
+            $name,
+            $email,
             $hash,
             'token'
         );
-        $user->confirmSignUp();
-        $user->changeRole(Role::admin());
-        $manager->persist($user);
-        $manager->flush();
     }
+    private function createSignedUpByNetwork(Name $name, string $network, string $identity): User
+    {
+        return User::signUpByNetwork(
+            Id::next(),
+            new \DateTimeImmutable(),
+            $name,
+            $network,
+            $identity
+        );
+    }
+
 }
