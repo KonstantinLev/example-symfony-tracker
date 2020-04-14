6f85f30036846b55889119cec09ebb1aeeb4225a
Konstantin <levachin@luckandroll.com>
2020-04-14 15:39:35 +0300

    Refactored group fetching

--- manager/src/ReadModel/Work/Members/GroupFetcher.php
+++ manager/src/ReadModel/Work/Members/GroupFetcher.php
@@ -26,7 +26,8 @@ class GroupFetcher
             ->orderBy('name')
             ->execute();
 
-        return array_column($stmt->fetchAll(FetchMode::ASSOCIATIVE), 'name', 'id');
+        //return array_column($stmt->fetchAll(FetchMode::ASSOCIATIVE), 'name', 'id');
+        return $stmt->fetchAll(\PDO::FETCH_KEY_PAIR);
     }
 
 
